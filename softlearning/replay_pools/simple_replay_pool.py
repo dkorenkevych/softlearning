7ca9beb115011b28f5dd831ea623b1b90b4a7ba6
Dmytro <dmytro.korenkevych@kindred.ai>
2019-07-10 12:00:09 -0400

    added prefetching

--- softlearning/replay_pools/simple_replay_pool.py
+++ softlearning/replay_pools/simple_replay_pool.py
@@ -2,7 +2,9 @@ from collections import defaultdict
 
 import numpy as np
 from gym.spaces import Box, Dict, Discrete
-
+from multiprocessing import Process, Array, Value, Lock
+from rl_experiments.data_collection.scan_data_db import DBManager
+import time
 from .flexible_replay_pool import FlexibleReplayPool
 
 
@@ -34,15 +36,16 @@ def normalize_observation_fields(observation_space, name='observations'):
 
 
 class SimpleReplayPool(FlexibleReplayPool):
-    def __init__(self, observation_space, action_space, img_dim=(0,), db_manager=None, *args, **kwargs):
+    def __init__(self, observation_space, action_space, batch_size, prefetch_size=20, img_dim=(0,), db_manager=None, *args, **kwargs):
         self._observation_space = observation_space
         self._action_space = action_space
         self.img_dim = img_dim
         self.db = db_manager
-
+        self.prefetch_size = prefetch_size
         observation_fields = normalize_observation_fields(observation_space)
         observation_fields['observations']['shape'] = (observation_space.shape[-1] - np.product(self.img_dim),)
-        observation_fields['images'] = {'dtype': 'uint8', 'shape': (np.product(self.img_dim),)}
+        observation_fields['observations']['ctype'] = 'f'
+        observation_fields['images'] = {'dtype': 'uint8', 'ctype': 'B', 'shape': (np.product(self.img_dim),)}
         # It's a bit memory inefficient to save the observations twice,
         # but it makes the code *much* easier since you no longer have
         # to worry about termination conditions.
@@ -56,22 +59,71 @@ class SimpleReplayPool(FlexibleReplayPool):
             **{
                 'actions': {
                     'shape': self._action_space.shape,
-                    'dtype': 'float32'
+                    'dtype': 'float32',
+                    'ctype': 'f'
                 },
                 'rewards': {
                     'shape': (1, ),
-                    'dtype': 'float32'
+                    'dtype': 'float32',
+                    'ctype': 'f'
                 },
                 # self.terminals[i] = a terminal was received at time i
                 'terminals': {
                     'shape': (1, ),
-                    'dtype': 'bool'
+                    'dtype': 'bool',
+                    'ctype': 'B'
                 },
             }
         }
-
+        self.field_params = fields
+        self.batch_data = {
+                'rewards': [],
+                'actions': [],
+                'terminals': [],
+                "observations": [],
+                "next_observations": [],
+                "images": [],
+                "next_images": []
+        }
+        for modality in self.batch_data.keys():
+            for _ in range(self.prefetch_size):
+                self.batch_data[modality].append(np.frombuffer(Array(fields[modality]['ctype'],
+                                                                     int(batch_size * np.product(fields[modality]['shape']))).get_obj(),
+                                                       dtype=fields[modality]['dtype']).reshape((batch_size, ) + fields[modality]['shape']))
         super(SimpleReplayPool, self).__init__(
             *args, fields_attrs=fields, **kwargs)
+
+        self.read_buffer_p = Value('i', 0)
+        self.write_buffer_p = Value('i', 0)
+        self.fresh_data = Value('i', 0)
+        self._access_lock = Lock()
+        self._fetch_loop = Process(target=self.fetch_loop, args=(batch_size,))
+        self._fetch_loop.start()
+
+    def fetch_loop(self, batch_size, field_name_filter=None):
+        db = DBManager(table='sac_pool3')
+        while True:
+            try:
+                if self.fresh_data.value < self.prefetch_size:
+                    time.sleep(0.5)
+                    print("prefetching a batch", self.fresh_data.value)
+                    random_indices = np.random.choice(range(db.last_id), batch_size, replace=False)
+                    data = self.batch_by_indices_db(db,
+                                             random_indices, field_name_filter=field_name_filter)
+                    buffer_pointer = self.write_buffer_p.value
+                    for modality in data:
+                        np.copyto(self.batch_data[modality][buffer_pointer], data[modality].astype(self.field_params[modality]['dtype']))
+                    self.write_buffer_p.value = (buffer_pointer + 1) % self.prefetch_size
+                    self._access_lock.acquire()
+                    self.fresh_data.value += 1
+                    self._access_lock.release()
+                time.sleep(0.1)
+            except:
+                print("Couldn't get new data, reconnecting")
+                db.cnx, db.cursor = db.make_connection()
+                time.sleep(1.0)
+
+
 
     def add_samples_base(self, samples):
         field_names = list(samples.keys())
@@ -87,10 +139,26 @@ class SimpleReplayPool(FlexibleReplayPool):
             assert values.shape[0] == num_samples
             self.fields[field_name][index] = values
 
-    def random_batch(self, batch_size, field_name_filter=None, **kwargs):
+    def random_batch_old(self, batch_size, field_name_filter=None, **kwargs):
         random_indices = np.random.choice(range(self.db.last_id), batch_size, replace=False)
-        return self.batch_by_indices_db(
+        return self.batch_by_indices_db(self.db,
             random_indices, field_name_filter=field_name_filter, **kwargs)
+
+    def random_batch(self, batch_size, field_name_filter=None, **kwargs):
+        while True:
+            if self.fresh_data.value <= 0:
+                print("No fresh data available, waiting", self.fresh_data.value)
+                time.sleep(0.1)
+                continue
+            buffer_pointer = self.read_buffer_p.value
+            batch_data = {}
+            for modality in self.batch_data:
+                batch_data[modality] = self.batch_data[modality][buffer_pointer].copy()
+            self.read_buffer_p.value = (buffer_pointer + 1) % self.prefetch_size
+            self._access_lock.acquire()
+            self.fresh_data.value -= 1
+            self._access_lock.release()
+            return batch_data
 
 
     def add_samples(self, samples):
@@ -142,10 +210,11 @@ class SimpleReplayPool(FlexibleReplayPool):
         }
 
     def batch_by_indices_db(self,
+                         db,
                          indices,
                          field_name_filter=None,
                          observation_keys=None):
-        data = self.db.get_samples_by_id(indices)
+        data = db.get_samples_by_id(indices)
 
         field_names = self.field_names
         if field_name_filter is not None:
